7c9ebf1218c232c7082e121c9c9ef37964886575
Feelan <panfilovich1@gmail.com>
2022-06-09 21:38:46 +0300

    1

---
+++ С++/Inverted drum fill volume/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <math.h>
+
+#define PI 3.14159265
+
+using namespace std;
+
+int main() {
+	// ввод первоначальных данных
+	double r, l, h;
+	cout << "r = ";
+	cin >> r;
+	cout << "l =  ";
+	cin >> l;
+	cout << "h = ";
+	cin >> h;
+
+	// если уровень воды больше либо равна двум радиусам
+	if (h >= 2 * r) {
+		cout << "V = " << PI * pow(r, 2) * l;
+	}
+	// если уровень воды меньше либо равен 0
+	else if (h <= 0 || r <= 0 || l <= 0) {
+		cout << "V = 0";
+	}
+	// если уровень воды на середине
+	else if (h == r) {
+		cout << "V = " << PI * pow(r, 2) * l / 2;
+	}
+	// если уровень воды выше середины
+	else if(h > r){
+		double a = h - r;
+		double b = sqrt((pow(r, 2) - pow(a, 2)));
+		double u = 2 * asin(b / r);
+		cout << "V = " << (((PI * pow(r, 2)) - (pow(r, 2) * (u - sin(u)))) * l) / 2;
+
+	}
+	// если уровень воды меньше радиуса
+	else {
+		double a = h - r;
+		double b = sqrt((pow(r, 2) - pow(a, 2)));
+		double u = 2 * asin(b / r);
+		cout << "V = " << (pow(r, 2) * (u - sin(u))) * l / 2;
+	}
+	cout << endl;
+	system("pause");
+	return 0;
+}
---
+++ С++/Inverted drum fill volume/README.md
@@ -0,0 +1,7 @@
+# Inverted drum fill volume
+
+In this order, it would be necessary to implement finding the volume of a barrel lying on its side.
+The difficulty was that it was necessary to provide for several options for the water level.
+
+The most important functional is the calculation of the area of a circle segment
+
